0ea6565200009ec83e15b5a791fe2351ad0e6c0b
zhangxuepeng <3238811454@qq.com>
2020-11-13 13:41:19 +0800

    090

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+   public function reg(){
+   			echo 123;
+   }
+
+   public function login(){
+   		return view('admin.admin.login');
+   }
 }
