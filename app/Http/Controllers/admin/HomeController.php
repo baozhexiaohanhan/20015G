6b3507e8186a1b31b8f83c3de23985a58498a58b
liu <2938937056@qq.com>
2020-11-13 15:04:56 +0800

    ssd

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     // 后台首页
-    public function index(){
-        return view("admin.home.index");
+    public function admin(){
+        return view("admin.home.admin");
     }
 }
