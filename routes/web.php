9db4aec1fe7308de6e66f343355dc307da5162b3
guo <3298287454@qq.com>
2020-11-13 15:06:13 +0800

    路由

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,16 @@ Route::get('/','Index\IndexController@index');//首页
 Route::get('/reg','admin\LoginController@reg');//首页
 
 Route::get('/login','admin\LoginController@login');//首页
+//广告管理
+Route::prefix("/ad")->group(function(){
+	//广告位
+    Route::get("/create","Admin\AdController@create");
+    Route::post("/store","Admin\AdController@store");
+    Route::get("/","Admin\AdController@index");
+    //广告
+    Route::get("/position/create","Admin\PositionController@create");
+    Route::post("/position/store","Admin\PositionController@store");
+    Route::get("/position","Admin\PositionController@index");
+    Route::get("/position/{position_id}","Admin\PositionController@showads");
+    Route::get("/position/createhtml/{position_id}","Admin\PositionController@createhtml");
+});
