436608ed3c58048c4fa8ab65afcee5f4d8ed05b8
zhangxuepeng <3238811454@qq.com>
2020-11-16 08:08:21 +0800

    23232

--- routes/web.php
+++ routes/web.php
@@ -24,31 +24,31 @@ Route::get('/login','Admin\LoginController@login');//首页
 //广告管理
 Route::prefix("ad")->group(function(){
 	//广告
-    Route::get("/create","Admin\AdController@create");
-    Route::post("/store","Admin\AdController@store");
-    Route::get("/","Admin\AdController@index");
-    Route::post('/upload','Admin\AdController@upload');
-    Route::get("/destroy/{id}","Admin\AdController@destroy");
-    Route::get("change","Admin\AdController@change");
+    Route::any("/create","Admin\AdController@create");
+    Route::any("/store","Admin\AdController@store");
+    Route::any("/","Admin\AdController@index");
+    Route::any('/upload','Admin\AdController@upload');
+    Route::any("/destroy/{id}","Admin\AdController@destroy");
+    Route::any("change","Admin\AdController@change");
     //广告位
-    Route::get("/position/create","Admin\PositionController@create");
-    Route::post("/position/store","Admin\PositionController@store");
-    Route::get("/position","Admin\PositionController@index");
-    Route::get("/position/{position_id}","Admin\PositionController@showads");
-    Route::get("/position/createhtml/{position_id}","Admin\PositionController@createhtml");
-    Route::get("/position/destroy/{id}","Admin\PositionController@destroy");
-    Route::get("/position/edit/{id}","Admin\PositionController@edit");
-    Route::post("/position/update/{id}","Admin\PositionController@update");
+    Route::any("/position/create","Admin\PositionController@create");
+    Route::any("/position/store","Admin\PositionController@store");
+    Route::any("/position","Admin\PositionController@index");
+    Route::any("/position/{position_id}","Admin\PositionController@showads");
+    Route::any("/position/createhtml/{position_id}","Admin\PositionController@createhtml");
+    Route::any("/position/destroy/{id}","Admin\PositionController@destroy");
+    Route::any("/position/edit/{id}","Admin\PositionController@edit");
+    Route::any("/position/update/{id}","Admin\PositionController@update");
 });
 Route::prefix('brand')->group(function(){
-    Route::get('/create','Admin\BrandController@create')->name('brand.create');
-    Route::post('/store','Admin\BrandController@store');
-    Route::get('/','Admin\BrandController@index')->name('brand');
-    Route::post('/upload','Admin\BrandController@upload');
-    Route::get('/edit/{brand_id}','Admin\BrandController@edit')->name('brand.edit');
-    Route::post('/update/{brand_id}','Admin\BrandController@update');
-    Route::get('/delete/{brand_id?}','Admin\BrandController@destroy');
-    Route::get('/change','Admin\BrandController@change');
+    Route::any('/create','Admin\BrandController@create')->name('brand.create');
+    Route::any('/store','Admin\BrandController@store');
+    Route::any('/','Admin\BrandController@index')->name('brand');
+    Route::any('/upload','Admin\BrandController@upload');
+    Route::any('/edit/{brand_id}','Admin\BrandController@edit')->name('brand.edit');
+    Route::any('/update/{brand_id}','Admin\BrandController@update');
+    Route::any('/delete/{brand_id?}','Admin\BrandController@destroy');
+    Route::any('/change','Admin\BrandController@change');
 });
 Route::any('/admins','Admin\HomeController@admins');//后台首页
 
