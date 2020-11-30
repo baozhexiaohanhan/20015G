a64c38e16fc521db4a9cf7cddd30fa7bde119a88
liu <2938937056@qq.com>
2020-11-30 09:15:07 +0800

    ss

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -11,8 +11,8 @@ class IndexController extends Controller
 {
     public function index(){
 		
-		$user_id = Redis::hmget("admin",["user_id","user_name"]);
-		dd($user_id);
+		// $user_id = Redis::hmget("admin",["user_id","user_name"]);
+		// dd($user_id);
 
     	//首页推荐位
     	$url='http://www.2001api.com/domain/index';
